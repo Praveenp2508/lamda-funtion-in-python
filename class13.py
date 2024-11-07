3b67a0bfe887c60234f14c073de4786690967ba5
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-11-07 16:34:45 +0530

    Add files via upload

---
+++ class13.py
@@ -0,0 +1,21 @@
+#lamda funtion
+    #syntex:
+        #ref_var=lambda x:expression
+
+# a=lambda x:x*2
+# print(a(10))
+#
+# sq=lambda x:x**2
+# print(sq(5))
+
+# x=input('enter')
+# lst=(map(int,x.split()))
+# print(lst)
+
+# print(list(filter(lambda x:x%2==0,range(0,51))))
+#
+# from functools import reduce
+# l=[10,20,30,40,50]
+# s=reduce((lambda x,y: x if x>y else y),l)
+# print(s)
+
